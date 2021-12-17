b8dda18be6db7e3019edc359f4f39ced4849640b
Anthony Nguyen <86622824+anthonynguyen1575@users.noreply.github.com>
2021-12-16 18:32:42 -0800

    Update Web-URL.h

--- Web-URL.h
+++ Web-URL.h
@@ -11,11 +11,9 @@ public:
 	std::string myPath;
 };
 
-std::string getURL(const URL url);
-std::string getScheme(const URL url);
-std::string getAuthority(const URL url);
-std::string getPath(const URL url);
-
-void printURLAndComponents(const URL url);
+std::string getURL(const URL& url);
+std::string getScheme(const URL& url);
+std::string getAuthority(const URL& url);
+std::string getPath(const URL& url);
 
 std::ostream& operator<<(std::ostream& os, const URL& url);
